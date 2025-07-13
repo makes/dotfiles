fbd642f77830c04850dbd159bd40316e1b3fddee
Simo Mäkelä <simo.makela@thl.fi>
2025-07-13 05:55:08 +0300

    updates from work machine

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,21 @@ $WORKSTATIONPROFILE = Join-Path $PROFILEPATH "workstation.ps1"
 $env:EDITOR = "nvim"
 $env:PAGER = "less"
 
-if ($host.Name -eq 'ConsoleHost')
-{
+Remove-Alias -Name ls -ErrorAction SilentlyContinue
+Set-Alias ll Get-ChildItem
+Function ls {
+    param (
+        [switch]$l
+    )
+    if ($l) {
+        Get-ChildItem @args
+    }
+    else {
+        (Get-ChildItem @args).Name
+    }
+}
+
+if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadLine
     Set-PSReadLineOption -PredictionSource History
     Set-PSReadLineOption -PredictionViewStyle InlineView #ListView
@@ -19,7 +32,8 @@ Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
 
 if (Get-Command Import-VisualStudioVars) {
     Import-VisualStudioVars
-} else {
+}
+else {
     Write-Host "PowerShell Community Extensions missing. Installation:"
     Write-Host "Install-Module -Name Pscx -AllowPrerelease"
     Write-Host "Install-Module -Name VSSetup"
@@ -38,13 +52,21 @@ Function nvimc { nvim $env:LOCALAPPDATA\nvim\lua }
 
 Set-Alias docker podman
 Function gitbash { sh --login }
-Function notes { cd $home/codes/notes && nvim . && cd - }
+if (Test-Path -Path alias:diff) { Remove-Item alias:diff -Force }
+Function diff { diff.exe --color $args }
+Function notes { Set-Location $home/codes/notes && nvim . && Set-Location - }
 Function updateconda { conda update -n base -c defaults conda }
 Function gcs([string]$param) { gh copilot suggest $param }
 Function gce([string]$param) { gh copilot explain $param }
 Function gs { git status }
 
-Function Refresh-Path { $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") } 
+Function Update-Path { $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") } 
+
+Update-Path
+
+# uv autocomplete
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
 
 # load workstation-specific config
 if (Test-Path -Path $WORKSTATIONPROFILE) { . $WORKSTATIONPROFILE }
